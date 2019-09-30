1c1254a0442282be6113e273c94334ba6b2bba03
Larkin <larkiningram@gmail.com>
2019-09-30 13:51:01 -0500

    got movie function working

--- liri.js
+++ liri.js
@@ -22,20 +22,19 @@ function switchCom() {
     switch (command) {
         case "concert-this":
             concert(userInput);
-            // log(log_this);
+            // write(log_this);
             break;
         case "spotify-this-song":
             spot(userInput);
-            write(log_this);
-            // log(log_this);
+            // write(log_this);
             break;
         case "movie-this":
             movie(userInput);
-            // log(log_this);            
+            // write(log_this);
             break;
         case "do-what-it-says":
             doIt();
-            // log(log_this);
+            // write(log_this);
             break;
     }
 };
@@ -43,35 +42,40 @@ function switchCom() {
 switchCom();
 
 
-// concert
+// concert -- done
 function concert() {
 
+    var artist = process.argv.slice(3).join(" ");
     var query = process.argv.slice(3).join("%20");
 
-
-    var queryURL = "https://rest.bandsintown.com/artists/"  + query + "/events?app_id=codingbootcamp"
+    var queryURL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp"
     console.log(queryURL);
 
     axios.get(queryURL).then(function (response) {
 
         var venueName = response.data[0].venue.name;
         var venueLocation = (response.data[0].venue.city + ",", response.data[0].venue.country);
-        var year = (response.data[0].datetime.slice(0,4));
-        var month = (response.data[0].datetime.slice(5,7));
-        var day = (response.data[0].datetime.slice(8,10));
+        var year = (response.data[0].datetime.slice(0, 4));
+        var month = (response.data[0].datetime.slice(5, 7));
+        var day = (response.data[0].datetime.slice(8, 10));
         var date = month + "/" + day + "/" + year;
 
         console.log("Venue Name: ", venueName);
         console.log("Venue Loctation: ", venueLocation);
         console.log("Concert Date: ", date);
+
+
     })
+    log_this.push([command, artist]);
+    return log_this
+
 };
 
 
 // spotify -- done
 function spot() {
     var searchTrack;
-    
+
     if (process.argv.length === 3) {
         searchTrack = "Ace of Base The Sign"
     }
@@ -81,26 +85,50 @@ function spot() {
     spotify.search({
         type: 'track',
         query: searchTrack
-    })
-        .then(function (data) {
-            var artistName = data.tracks.items[0].album.artists[0].name;
-            var songName = searchTrack;
-            var spotifyLink = data.tracks.items[0].album.artists[0].external_urls.spotify;
-            var albumName = data.tracks.items[0].album.name;
+    }).then(function (data) {
+        var artistName = data.tracks.items[0].album.artists[0].name;
+        var songName = searchTrack;
+        var spotifyLink = data.tracks.items[0].album.artists[0].external_urls.spotify;
+        var albumName = data.tracks.items[0].album.name;
 
-            console.log("Song: ", songName);
-            console.log("Artist: ", artistName);
-            console.log("Album: ", albumName);
-            console.log("Spotify Link: ", spotifyLink);
+        console.log("Song: ", songName);
+        console.log("Artist: ", artistName);
+        console.log("Album: ", albumName);
+        console.log("Spotify Link: ", spotifyLink);
 
-            log_this.push([command, songName]);
-            console.log(log_this);
-            return log_this
-        });
+    });
+    log_this.push([command, searchTrack]);
+    return log_this
 };
 
 // movie
 function movie() {
+
+    var movieName = process.argv.slice(3).join("+");
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryUrl).then(function(response) {
+        
+        var title = response.data.Title; 
+        var year = response.data.Year;
+        var imdbRating = response.data.Ratings[0].Value;
+        var tomatoesRating = response.data.Ratings[1].Value;
+        var country = response.data.Country;
+        var language = response.data.Language;
+        var plot = response.data.Plot;
+        var actors = response.data.Actors;
+        
+        console.log("Title: ", title);
+        console.log("Released: ", year);
+        console.log("IMDB Rating: ", imdbRating);
+        console.log("Rotten Tomatoes Rating: ", tomatoesRating);
+        console.log("Country: ", country);
+        console.log("Language: ", language);
+        console.log("Plot: ", plot);
+        console.log("Actors: ", actors);
+    
+    })
 
 };
 
@@ -126,13 +154,11 @@ function doIt() {
 };
 
 function write() {
-    console.log(log_this)
-    fs.writeFile("log.txt", log_this, function (err) {
+    fs.appendFile("log.txt", "," + log_this, function (err) {
         if (err) {
             console.log("error: ", err);
         }
 
         console.log("log.txt was updated!");
-
     })
 };
